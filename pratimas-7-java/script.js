c864518abc963c6d52700b69d9301cf78554d3cd
Name Surname <84101522+raimarkas@users.noreply.github.com>
2021-06-28 21:40:48 +0300

    1

---
+++ pratimas-7-java/script.js
@@ -0,0 +1,11 @@
+const myage = 29
+console.log ("myage")
+
+const car = "BMW";
+if (car === "Voks" || car === "Mini") {
+  console.log("Volks group");
+} else if (car === "Volvo" || car === "BMW") {
+  console.log("BMW group");
+} else {
+  console.log("Nepriklauso ne vienai");
+}
