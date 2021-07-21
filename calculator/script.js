a32f141a65f218b127f0da1a678a93aa530cbb0d
Name Surname <84101522+raimarkas@users.noreply.github.com>
2021-07-21 18:46:16 +0300

    day2

--- calculator/script.js
+++ calculator/script.js
@@ -1,29 +1,32 @@
-let val1 = parseInt(document.getElementById('nr1').value)
-let val2 = parseInt(document.getElementById('nr2').value)
-const operation = document.getElementById('operators')
+let val1 = parseFloat(document.getElementById('nr1').value)
+let val2 = parseFloat(document.getElementById('nr2').value)
+const data = document.getElementById('resultData')
+const operation = document.querySelectorAll('#operators')
 let calculate
 
-const buttons = document.querySelectorAll('#operators')
-buttons.forEach(button => {
+operation.forEach(button => {
     button.addEventListener('click', event => {
-        console.log( event.target.id );
-        resultData.appendChild(Calculation).value
+    console.log( event.target.id );
+
+    if (event.target.id === 'btn-sum'){
+        calculate = val1 + val2
+        console.log(calculate)}
+    else if (event.target.id === 'btn-sub'){
+        calculate.value = val1 - val2
+        console.log(calculate)}
+    else if (event.target.id === 'btn-mul'){
+         calculate = val1 * val2
+    }
+    else if (event.target.id === 'btn-div'){
+         calculate = val1 / val2
+    }
+    else if (event.target.id === 'btn-mod'){
+         calculate = val1 % val2
+     } 
+    else (event.target.id === 'btn-clear');{
+        document.getElementById('nr1').value = ' '
+        document.getElementById('nr2').value = ' '
+    }
+    console.log(calculate)
    });
 });
-
-function Calculation(){
-    if (buttons.target.id === 'btn-sum'){
-        calculate = val1 + val2
-    } else if (buttons.target.id === 'btn-sub'){
-        calculate = val1 - val2
-    } else if (buttons.target.id === 'btn-mul'){
-        calculate = val1 * val2
-    } else if (buttons.target.id === 'btn-div'){
-        calculate = val1 / val2
-    } else if (buttons.target.id === 'btn-mod'){
-        calculate = val1 % val2
-    } else (buttons.target.id === 'btn-clear');{
-        document.getElementById('nr1, nr2').value = ''
-}}
-
-resultData.appendChild(calculate).value
