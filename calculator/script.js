e3d997bc1d1af678f3501b071ad8c76f63d488ee
Name Surname <84101522+raimarkas@users.noreply.github.com>
2021-07-21 19:57:19 +0300

    done

--- calculator/script.js
+++ calculator/script.js
@@ -1,32 +1,34 @@
-let val1 = parseFloat(document.getElementById('nr1').value)
-let val2 = parseFloat(document.getElementById('nr2').value)
-const data = document.getElementById('resultData')
+const result = document.getElementById('result')
 const operation = document.querySelectorAll('#operators')
 let calculate
 
 operation.forEach(button => {
     button.addEventListener('click', event => {
     console.log( event.target.id );
-
-    if (event.target.id === 'btn-sum'){
+const btn = event.target.id
+let val1 = parseFloat(document.getElementById('nr1').value)
+let val2 = parseFloat(document.getElementById('nr2').value)
+    if (btn === 'btn-sum'){
         calculate = val1 + val2
-        console.log(calculate)}
-    else if (event.target.id === 'btn-sub'){
-        calculate.value = val1 - val2
-        console.log(calculate)}
-    else if (event.target.id === 'btn-mul'){
+    }
+    else if (btn === 'btn-sub'){
+        calculate = val1 - val2
+    }
+    else if (btn === 'btn-mul'){
          calculate = val1 * val2
     }
-    else if (event.target.id === 'btn-div'){
+    else if (btn === 'btn-div'){
          calculate = val1 / val2
     }
-    else if (event.target.id === 'btn-mod'){
+    else if (btn === 'btn-mod'){
          calculate = val1 % val2
      } 
-    else (event.target.id === 'btn-clear');{
+    else (btn === 'btn-clear');{
         document.getElementById('nr1').value = ' '
         document.getElementById('nr2').value = ' '
     }
     console.log(calculate)
+    result.innerHTML = calculate
    });
 });
+
